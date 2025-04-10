08a6cf2cace939e1efe30687e6b6e5dbeff6a624
CallieBone <143444240+CallieBone@users.noreply.github.com>
2025-04-10 04:58:20 +0000

    Updated 2

--- bruce-url/js/url.js
+++ bruce-url/js/url.js
@@ -1,5 +1,3 @@
-// JavaScript File: riverGaugeData.js
-
 // Function to fetch river gauge data
 function fetchRiverGaugeData() {
     const apiUrl = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=07055646,070556680,07055680,07055780&period=P7D&siteStatus=active&parameterCd=00065';
@@ -10,7 +8,7 @@ function fetchRiverGaugeData() {
                 throw new Error("Network response was not ok " + response.statusText);
             }
             // This will Check if the response is ok (status in the range 200-299), the rror will come up if the response is not ok, like demonstrated in the code I made :DDDDDD
-            return response.json(); // Parse the JSON data
+            return response.json(); // Parse the JSON data {Parse means to convert the data from one format to another, in this case, from JSON string to a JavaScript object}
         })
         .then(data => {
             // This is where you can handle the data returned from the API
