b659154edcf694a1d3e2884d54618d8bc8154fa2
CallieBone <143444240+CallieBone@users.noreply.github.com>
2025-04-10 04:53:39 +0000

    Updated

--- bruce-url/js/url.js
+++ bruce-url/js/url.js
@@ -1,25 +1,28 @@
-// URL to fetch the river data
-const apiUrl = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=07055646,070556680,07055680,07055780&period=P7D&siteStatus=active&parameterCd=00065';
+// JavaScript File: riverGaugeData.js
 
-// Function to fetch and display river data
-function fetchRiverData() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', apiUrl, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                // Parse JSON response
-                const riverData = JSON.parse(xhr.responseText);
-                console.log('River Data:', riverData);
-                // Show river data in an alert message
-                alert('River Data: ' + JSON.stringify(riverData, null, 2));
-            } else {
-                console.error('Error fetching river data. Status:', xhr.status);
+// Function to fetch river gauge data
+function fetchRiverGaugeData() {
+    const apiUrl = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=07055646,070556680,07055680,07055780&period=P7D&siteStatus=active&parameterCd=00065';
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok " + response.statusText);
             }
-        }
-    };
-    xhr.send();
+            // This will Check if the response is ok (status in the range 200-299), the rror will come up if the response is not ok, like demonstrated in the code I made :DDDDDD
+            return response.json(); // Parse the JSON data
+        })
+        .then(data => {
+            // This is where you can handle the data returned from the API
+            console.log("River Gauge Data:", data);
+            alert("River Gauge Data Retrieved! Check the console for details.");
+        })
+        .catch(error => {
+            // This handle any errors that may occur during the fetch
+            console.error("There has been a problem with your fetch operation:", error);
+            alert("Error fetching river gauge data. See console for details.");
+            // This "alert" is to inform the user that there was an error fetching the data; Could possibly see it if we disconnect the wifi or if the API is down
+        });
 }
 
-// Call the function to fetch data
-fetchRiverData();
+fetchRiverGaugeData();
